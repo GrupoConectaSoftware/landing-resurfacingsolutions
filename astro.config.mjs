c3c14e8b14f516faf8e455f078bc87aa454ec4b9
GrupoConectaSoftware <grupoconectasoftware@gmail.com>
2026-07-31 12:09:14 -0500

    FRT001
    Creacion de menú de navegación final con sticky, neón y responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import alpinejs from "@astrojs/alpinejs";
+import icon from "astro-icon";
 
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
 
-    integrations: [alpinejs()],
+    integrations: [alpinejs(), icon()],
 });
